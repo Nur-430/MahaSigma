641867ae0905f9d33873b22759ff7359413281ff
Nur-430 <nurokhim430@gmail.com>
2026-09-23 16:16:33 +0700

    Mengembalikan namespace ke com.example dan menghapus import manual

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 }
 
 android {
-  namespace = "com.nurokhim.mahasigma"
+  namespace = "com.example"
   compileSdk { version = release(36) { minorApiLevel = 1 } }
 
   defaultConfig {
--- app/src/main/java/com/example/ui/components/AboutAppComponents.kt
+++ app/src/main/java/com/example/ui/components/AboutAppComponents.kt
@@ -1,6 +1,5 @@
 package com.example.ui.components
 
-import com.nurokhim.mahasigma.R
 import android.app.PendingIntent
 import android.appwidget.AppWidgetManager
 import android.content.ClipData
