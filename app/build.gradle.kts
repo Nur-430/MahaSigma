455de4af0eb41ea650efd525235c7703605a81a2
Nur-430 <nurokhim430@gmail.com>
2026-09-23 12:36:47 +0700

    fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,11 +10,11 @@ plugins {
 }
 
 android {
-  namespace = "com.example"
+  namespace = "com.nurokhim.mahasigma"
   compileSdk { version = release(36) { minorApiLevel = 1 } }
 
   defaultConfig {
-    applicationId = "com.aistudio.mahasigma.kpxw"
+    applicationId = "com.nurokhim.mahasigma"
     minSdk = 24
     targetSdk = 36
     versionCode = 1
